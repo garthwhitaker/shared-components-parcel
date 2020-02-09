9a8c62fd04d6c6f6af41cd365a49d3ac94c8a471
Garth Whitaker <garth.whitaker@gmail.com>
2020-02-09 17:31:42 +0000

    Adding material ui to library

--- examples/index.js
+++ examples/index.js
@@ -1,4 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Button from "../src/Button";
 
-ReactDOM.render(<div>Hahah</div>, document.getElementById("root"));
+const App = () => (
+  <React.Fragment>
+    <h1>Just a page showing all components.</h1>
+    <h2>I will be creating story book for all the components</h2>
+    <Button>Hello</Button>
+  </React.Fragment>
+);
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
-
-const Button = () => {
-  return <div>Button</div>;
+import MuiButton from "@material-ui/core/Button";
+const Button = props => {
+  return <MuiButton {...props} />;
 };
 
 export default Button;
